de613bf575155a6bbd491ab3aa9d65330ddc4165
lewis-quinn-bright-signals <lewis.quinn@brightsignals.co.uk>
2023-02-21 15:00:38 +0000

    first comit

---
+++ components/categoryLinks.js
@@ -0,0 +1,84 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function CatLink() {
+    return (
+        <div className='category-lower  ds_pre-footer-background'>
+                  <div className='ds_wrapper'>
+                    <div className='ds_layout  ds_layout--category-list'>
+                      <div className='ds_layout__grid'>
+                        <div className='ds_category-list-container'>
+                          <ol className='ds_category-list  ds_category-list--grid'>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+
+                              <li className='ds_category-item'>
+                                <Link href={"#"} className="ds_category-item__link" data-navigation="category-item-1">
+                                  <h2 className='ds_category-item__title ds_!_margin-top--auto ds_!_margin-bottom--0 ds_!_display--flex'>
+                                    Test category
+                                  </h2>
+                                </Link>
+                              </li>
+                            
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '<designscotland>/styles/globals.scss'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
