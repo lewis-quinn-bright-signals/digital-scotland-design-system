c353063f1b1ef64b642e74d79c6bb95c678dab97
lewis-quinn-bright-signals <lewis.quinn@brightsignals.co.uk>
2023-02-21 16:53:34 +0000

    3

---
+++ out/_next/static/yiRF0nxk5cAQ1E3wfcbzR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-88302190634dcd53.js","static/chunks/pages/index-3198ff3e90ed3ff6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
